4969e806ca61be78038f8774053090b272f16cce
HardBoris <bgaibor03@gmail.com>
2022-04-21 19:35:27 -0400

    feat: pivots tables created

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,3 +3,6 @@ from .production_model import Production
 from .purchase_model import Purchase
 from .recipe_model import Recipe
 from .user_model import User
+from .recipe_ingredients_model import RecipeIngredient
+from .production_recipes_model import ProductionRecipe
+from .ingredients_purchase_model import IngredientsPurchase
---
+++ app/models/recipe_ingredients_model.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+
+from app.configs.database import db
+from sqlalchemy import Column, Float, ForeignKey, Integer, Numeric, String
+
+
+@dataclass
+class RecipeIngredient (db.Model):
+    ingredient_id: int
+    quantity: float
+    recipe_id: int
+
+    __tablename__ = "recipe_ingredients"
+
+    id = Column(Integer, primary_key = True)
+    recipe_id = Column(Integer, ForeignKey("recipes.recipe_id"), nullable=False)
+    ingredient_id = Column(Integer, ForeignKey("ingredients.ingredient_id"), nullable=False)
+    quantity = Column(Float(3), nullable = False)
+    
