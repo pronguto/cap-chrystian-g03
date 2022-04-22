7790bb6f4111683968864603c2ba558ec2810bf2
HardBoris <bgaibor03@gmail.com>
2022-04-21 20:18:06 -0400

    feat: relationships ok

--- app/models/recipe_model.py
+++ app/models/recipe_model.py
@@ -3,6 +3,8 @@ from dataclasses import dataclass
 from app.configs.database import db
 from sqlalchemy import Column, Integer, String
 
+from app.models.production_recipes_model import ProductionRecipe
+
 
 @dataclass
 class Recipe (db.Model):
@@ -14,3 +16,4 @@ class Recipe (db.Model):
     recipe_id = Column(Integer, primary_key = True)
     recipe_name = Column(String, nullable = False, unique = True)
     
+    production = db.relationship("Production", secondary = ProductionRecipe, backref = "recipe")
