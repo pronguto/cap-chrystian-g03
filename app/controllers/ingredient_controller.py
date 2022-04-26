f49ab3a60bb5b861b71defb4d14b9e6c23938a52
Gustavo Ribeiro Sergio <ribeiro.gustavo95@gmail.com>
2022-04-26 18:35:13 -0300

    create ingredients

--- app/controllers/ingredient_controller.py
+++ app/controllers/ingredient_controller.py
@@ -1,24 +1,35 @@
-from flask_jwt_extended import (
-    create_access_token,
-    jwt_required,
-    get_jwt_identity,
-)
+from app.configs.database import db
+from app.models.ingredient_model import Ingredient
+from flask import request, jsonify
+from flask_jwt_extended import get_jwt_identity, jwt_required
+from sqlalchemy.orm.session import Session
+from http import HTTPStatus
 
-@jwt_required()
+
+# @jwt_required()
 def ingredient_creator():
-    return {"msg": "ingredient creator"}
+    data= request.get_json()
+    session: Session= db.session()
+    ingredient: Ingredient= Ingredient(**data)
+    try:
+        session.add(ingredient)
+        session.commit()
+    except:
+        return {"msg": "error"}, HTTPStatus.BAD_REQUEST
+    return jsonify(ingredient), HTTPStatus.CREATED
 
-@jwt_required()
+# @jwt_required()
 def ingredient_loader():
+    
     return {"msg": "ingredient loader"}
 
 # def ingredient_by_name():
 #     return {"msg": "ingredient by name"}
 
-@jwt_required()
+# @jwt_required()
 def ingredient_updater():
     return {"msg": "ingredient updater"}
 
-@jwt_required()
+# @jwt_required()
 def ingredient_deleter():
     return {"msg": "ingredient deleter"}
