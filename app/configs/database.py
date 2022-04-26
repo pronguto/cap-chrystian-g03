d57b1a0d062b94766dee1382064fb9c923c77525
Filipe Melo <filipexpert63@gmail.com>
2022-04-26 17:19:43 -0300

    Update README

--- app/configs/database.py
+++ app/configs/database.py
@@ -8,6 +8,8 @@ db= SQLAlchemy()
 def init_app(app: Flask):
     app.config["SQLALCHEMY_DATABASE_URI"]= os.getenv("DATABASE_URI") 
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= False
+    app.config['JSON_SORT_KEYS'] = False
+
 
     db.init_app(app)
 
