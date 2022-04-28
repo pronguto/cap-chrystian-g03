168268b51cd6bcafe654839d1ccb544d86f09bec
Gustavo Ribeiro Sergio <ribeiro.gustavo95@gmail.com>
2022-04-28 20:42:35 -0300

    merging heads from migrates

---
+++ migrations/versions/e86e1e55c048_merging_heads.py
@@ -0,0 +1,24 @@
+"""merging heads
+
+Revision ID: e86e1e55c048
+Revises: 846668a39371, 3d88ba222821
+Create Date: 2022-04-28 20:41:34.990701
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e86e1e55c048'
+down_revision = ('846668a39371', '3d88ba222821')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
